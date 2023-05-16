5de7d8d37cfb3fc07087b8494773775596af7559
Gustavo Braga <gb95131aq@gmail.com>
2023-05-16 16:31:18 -0300

    Criado a classe AppError para o tratamento de erros e o Middleware no server para capturar os erros gerados

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const port = process.env.PORT || 3333;
 const app = express();
@@ -8,6 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+//Middleware para tratamento de erros
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  //Se o erro for uma instancia da classe para tratamento de erros
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+  }
+  //Se não for será um erro desconhecido
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(port, () => {
   console.log(`Executando na porta ${port}`);
