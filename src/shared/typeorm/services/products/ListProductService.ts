440edb5b8a441e44f07db4083ed231baa0b1b064
Gustavo Braga <gb95131aq@gmail.com>
2023-05-19 10:44:44 -0300

    Criado o serviço para listar os produtos

---
+++ src/shared/typeorm/services/products/ListProductService.ts
@@ -0,0 +1,14 @@
+import AppDataSource from '@shared/typeorm';
+import Product from '@shared/typeorm/entities/Product';
+
+class ListProductService {
+  public async execute(): Promise<Product[] | undefined> {
+    const productRepository = AppDataSource.getRepository(Product);
+
+    const products = await productRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
