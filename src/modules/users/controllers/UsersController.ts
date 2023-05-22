6eb6aaaca9db56cca7bb4f678d37db8778021474
Gustavo Braga <gb95131aq@gmail.com>
2023-05-22 17:52:00 -0300

    Criado o controller para User

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,19 @@
+import CreateUserService from '@shared/typeorm/services/users/CreateUserService';
+import ListUserService from '@shared/typeorm/services/users/ListUserService';
+import { Request, Response } from 'express';
+
+class UsersController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listUser = new ListUserService();
+    const listUsers = await listUser.execute();
+    return res.json(listUsers);
+  }
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+    const createUser = new CreateUserService();
+    const user = await createUser.execute({ name, email, password });
+    return res.json(user);
+  }
+}
+
+export default UsersController;
