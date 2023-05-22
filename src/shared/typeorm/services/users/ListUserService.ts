99d02b8a680934afc573dcc52d4aa2f1602112e8
Gustavo Braga <gb95131aq@gmail.com>
2023-05-22 16:32:38 -0300

    Criado serviço para listar usuarios

---
+++ src/shared/typeorm/services/users/ListUserService.ts
@@ -0,0 +1,14 @@
+import AppDataSource from '@shared/typeorm';
+import User from '@shared/typeorm/entities/User';
+
+class ListUserService {
+  public async execute(): Promise<User[] | undefined> {
+    const userRepository = AppDataSource.getRepository(User);
+
+    const users = await userRepository.find();
+
+    return users;
+  }
+}
+
+export default ListUserService;
