9fbee82dc322bd026744e179e7275202cb543a71
Gustavo Braga <gb95131aq@gmail.com>
2023-05-23 15:45:22 -0300

    Adicionado hash para a criação de senha do usuário

--- src/shared/typeorm/services/users/CreateUserService.ts
+++ src/shared/typeorm/services/users/CreateUserService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import AppDataSource from '@shared/typeorm';
 import User from '@shared/typeorm/entities/User';
 import IUser from '@shared/typeorm/interfaces/IUser';
+import { hash } from 'bcrypt';
 
 class CreateUserService {
   public async execute({ name, email, password }: IUser): Promise<User> {
@@ -12,8 +13,8 @@ class CreateUserService {
     if (emailExists) {
       throw new AppError('E-mail já cadastrado');
     }
-
-    const user = userRepository.create({ name, email, password });
+    const hashedPassw = await hash(password!, 8);
+    const user = userRepository.create({ name, email, password: hashedPassw });
     await userRepository.save(user);
     return user;
   }
