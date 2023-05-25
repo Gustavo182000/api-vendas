60774ae25dc54c44fbc27145b4b8453fd9cc3f81
Gustavo Braga <gb95131aq@gmail.com>
2023-05-25 11:12:50 -0300

    Adicionado JWT na criação de sessão

--- src/shared/typeorm/services/users/CreateSessionsService.ts
+++ src/shared/typeorm/services/users/CreateSessionsService.ts
@@ -3,9 +3,15 @@ import AppDataSource from '@shared/typeorm';
 import User from '@shared/typeorm/entities/User';
 import IUser from '@shared/typeorm/interfaces/IUser';
 import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionsService {
-  public async execute({ email, password }: IUser): Promise<User> {
+  public async execute({ email, password }: IUser): Promise<IResponse> {
     const userRepository = AppDataSource.getRepository(User);
     const user = await userRepository.findOne({ where: { email } });
 
@@ -16,7 +22,11 @@ class CreateSessionsService {
     if (!userCompare) {
       throw new AppError('Senha incorreta', 401);
     }
-    return user;
+    const token = sign({ nome: user.name, email: user.email }, '123456', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+    return { user, token };
   }
 }
 
