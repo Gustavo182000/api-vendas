e498df3698a23ea294ba4f168c432b97e71c3508
Gustavo Braga <gb95131aq@gmail.com>
2023-05-26 14:21:25 -0300

    Adicionado controller do Avatar

---
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -0,0 +1,14 @@
+import UpdateUserAvatarService from '@shared/typeorm/services/users/UpdateUserAvatarService';
+import { Request, Response } from 'express';
+class UserAvatarController {
+  public async update(req: Request, res: Response): Promise<Response> {
+    const updateAvatar = new UpdateUserAvatarService();
+    const user = updateAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file!.filename,
+    });
+    return res.json(user);
+  }
+}
+
+export default UserAvatarController;
