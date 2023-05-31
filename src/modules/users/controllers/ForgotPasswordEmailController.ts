40a905190f151f68a60a0b8aa204f22a857b1f59
Gustavo Braga <gb95131aq@gmail.com>
2023-05-31 20:59:56 -0300

    Criado controller para envio de email para recuperação de senha

---
+++ src/modules/users/controllers/ForgotPasswordEmailController.ts
@@ -0,0 +1,15 @@
+import SendForgotPasswordEmailService from '@shared/typeorm/services/users/SendForgotPasswordEmailService';
+import { Request, Response } from 'express';
+
+class ForgotPasswordEmailController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email } = req.body;
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService();
+    await sendForgotPasswordEmail.execute(email);
+
+    return res.status(204).json();
+  }
+}
+
+export default ForgotPasswordEmailController;
