ac77a03b431fb18ebdc904992f3df1ad3abac8eb
Gustavo Braga <gb95131aq@gmail.com>
2023-06-05 10:53:30 -0300

    Criado serviço para deletar um customer

---
+++ src/shared/typeorm/services/customers/DeleteCustomerService.ts
@@ -0,0 +1,27 @@
+import AppError from '@shared/errors/AppError';
+import AppDataSource from '@shared/typeorm';
+import Customers from '@shared/typeorm/entities/Customer';
+
+interface ICustommer {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteCustomerService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const customerRepository = AppDataSource.getRepository(Customers);
+    const customer = await customerRepository.findOne({ where: { id } });
+
+    if (!customer) {
+      throw new AppError('Cliente não encontrado');
+    }
+    await customerRepository.delete(customer);
+  }
+}
+
+export default DeleteCustomerService;
